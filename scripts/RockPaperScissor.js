22d96a1a5e3c0ff67694c8191c6a9a58562129bd
Acchu <acchu1915@gmail.com>
2023-09-08 15:43:35 +0530

    added who played css is still messed up

--- scripts/RockPaperScissor.js
+++ scripts/RockPaperScissor.js
@@ -1,10 +1,12 @@
 // Modify to appealing visuals later
 function getComputerChoice(){
+    if(round > 4) return;
     const checkValue = Math.floor(Math.random() * 3);
     return checkValue === 0 ? "rock":(checkValue === 1)? "scissors": "paper";
 }
 
-function resetScore(){
+function resetScore()
+{
         //if(round < 5) return;
         playerScore = 0;
         computerScore = 0;
@@ -12,32 +14,36 @@ function resetScore(){
         draw = 0;
         document.querySelector('img').classList.remove('show-when-win')
         updateScoreWebsite();
-    }
+}
     
-    function getWinner(){
-        const displayWinner = document.querySelector('#display-winner');  
-        if(playerScore === computerScore){
-            displayWinner.textContent = 'Draw';
-            return;
-        }
+function getWinner()
+{
+    const displayWinner = document.querySelector('#display-winner');  
+    if(playerScore === computerScore)
+    {
+        displayWinner.textContent = 'Draw';
+        return;
+    }
         // (playerScore > computerScore) ? displayWinner.textContent = 'Player Wins' : displayWinner.textContent = 'Cat Wins';
-        if(playerScore > computerScore){
-            displayWinner.textContent = 'Player Wins';
-            return;
-        }
-        else
-        {
+    if(playerScore > computerScore)
+    {
+        displayWinner.textContent = 'Player Wins';
+        return;
+     }
+    else
+    {
             displayWinner.textContent = 'Cat Wins';
             document.querySelector('img').classList.add('show-when-win')
-        }
     }
+}
     
-    function updateScoreWebsite(){
+function updateScoreWebsite()
+{
     const currentRound = document.querySelector('#round');
     const domPlayerScore = document.querySelector('#player-score');
     const domComputerScore = document.querySelector('#computer-score');
     const domDraw = document.querySelector('#draws');
-        
+    
     const displayWinner = document.querySelector('#display-winner');
     displayWinner.textContent = `Winner : UNDECIDED`;
     currentRound.textContent = `Round Number: ${round}`;
@@ -57,7 +63,9 @@ function updateScore(){
 
 function playRound(choice){
     const playerChoice = choice.target.id;
+    document.querySelector('.player-played').textContent = playerChoice.toUpperCase();
     const computerChoice = getComputerChoice();
+    document.querySelector('.computer-played').textContent = computerChoice.toUpperCase();
     if(computerChoice === playerChoice){
         draw++;
         updateScore();
